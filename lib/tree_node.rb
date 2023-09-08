7a066e6020e696af16e376023177014c141ddbb5
Jason Zhang <jasonzhang501@gmail.com>
2023-09-08 14:16:24 -0400

    Almost Done With Parent

--- lib/tree_node.rb
+++ lib/tree_node.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class PolyTreeNode
   
   attr_reader :value, :parent, :children 
@@ -6,5 +8,19 @@ class PolyTreeNode
     @value = value
     @parent = nil 
     @children = []
+  end
+
+  def parent=(parent_node)
+    # debugger
+    @parent = parent_node
+
+    parent_node.children.delete(self)
+
+    if self.parent == nil
+      @children = nil
+    elsif !parent_node.children.include?(self) 
+      parent_node.children << self
+    end
+
   end
 end
